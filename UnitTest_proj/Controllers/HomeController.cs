864e8e4d7bc4fbf045388ba9f21a2445551a10a2
leshashef <lesha-shevchenko2001@mail.ru>
2024-09-20 03:14:41 +0300

    Добавьте файлы проекта.

---
+++ UnitTest_proj/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTest_proj.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Hello!";
+            return View("Index");
+        }
+    }
+}
